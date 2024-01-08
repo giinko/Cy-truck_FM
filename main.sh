c8998d9a0fafc8ec9f0ebc795d4dbca639d738e1
max <ginko.opro@gmail.com>
2024-01-08 22:38:23 +0100

    amélioration des graphiques

--- main.sh
+++ main.sh
@@ -6,18 +6,9 @@ if [ "$#" -lt 2 ]; then
     exit 1
 fi
 
-# Vérifier et créer le dossier temp /!\ A REVOIR ICI CAR C4EST A LA FIN QU4IL FAUT SUPP MAIS CE TCHECK EST PAS MAL AUSSI
-if [ ! -d temp ]; then
-    mkdir temp
-else
-    rm -rf temp/*
-fi
-
-# Vérifier et créer le dossier images
-if [ ! -d images ]; then
-    mkdir images
-else
-    rm -rf images/*
+if [ "$#" -gt 2 ]; then
+    echo "Usage: $0 <chemin_fichier_csv> [-h | -d | -... ]"
+    exit 1
 fi
 
 # Vérifier si l'argument 1 est bien un fichier valide
@@ -26,6 +17,19 @@ if [ ! -f "$1" ]; then
     exit 1
 fi
 
+# Vérifier le dossier temp
+if [ ! -d temp ]; then
+    mkdir temp
+else
+    rm -rf temp/*
+fi
+
+# Vérifier le dossier images
+if [ ! -d images ]; then
+    mkdir images
+else
+    rm -rf images/*
+fi
 
 # Mesurer le temps avant le traitement
 start_time=$(date +%s)
@@ -45,29 +49,26 @@ case "$2" in
         data_csv="$1"  # Chemin vers le fichier CSV des conducteurs
 
         # Extraire les noms des conducteurs et le nombre de trajets
-        awk -F ";" '{count[$6]+=1} END {for (c in count) print c,";" count[c]}' "$data_csv" |
-        sort -t';' -k2 -n -r | head -10 > temp/conducteurs1.txt
-        tac temp/conducteurs1.txt > temp/conducteurs2.txt
+        awk -F';' '!seen[$1]++' "$data_csv" | awk -F ";" '{count[$6]+=1} END {for (c in count) print c,";" count[c]}' |
+        sort -t';' -k2,2nr | head -10 | tac > temp/conducteurs2.txt
 
         # Créer le graphique avec Gnuplot
         echo       "set terminal pngcairo enhanced font 'arial,10' size 800,600
                     set output 'images/conducteurs.png'
                     set datafile separator ';'
+                    unset key
 
                     set style fill solid
                     set title font 'Arial,18'
                     set title 'Nombres totales de trajets par chauffeurs'
-                    set margin 15,2,7,5
+                    set margin 24,2,3.5,3.5
 
-                    set ylab 'Noms des chauffeurs' font 'Arial, 15' offset -6,0
+                    set ylab 'Noms des chauffeurs' font 'Arial, 15' offset -1.8,0
                     set xlab 'Nombres de trajets' font 'Arial, 15' offset 2,0
 
-                    set xrange [3400:4600]
-                    set yrange [-0.5:8.5]
-                    set ytics rotate by 60 offset 1,.2 right
+                    set yrange [-0.75:9.75]
 
-
-                    plot 'temp/conducteurs2.txt' u (\$2/2):0:(\$2/2):(0.35/2.):ytic(1) w boxxy ti 'Trajet'" > temp/conducteurs.gp
+                    plot 'temp/conducteurs2.txt' u (\$2/2):0:(\$2/2):(0.75/2.):ytic(1) w boxxy ti 'Trajet' lc 'blue'" > temp/conducteurs.gp
         
         gnuplot temp/conducteurs.gp
         ;;
@@ -84,21 +85,19 @@ case "$2" in
         echo       "set terminal pngcairo enhanced font 'arial,10' size 800,600
                     set output 'images/distance.png'
                     set datafile separator ';'
+                    unset key
 
                     set style fill solid
                     set title font 'Arial,18'
                     set title 'Distance parcouru par chaque chauffeur'
-                    set margin 15,2,7,5
+                    set margin 24,2,3.5,3.5
 
-                    set ylab 'Noms des chauffeurs' font 'Arial, 15' offset -6,0
+                    set ylab 'Noms des chauffeurs' font 'Arial, 15' offset -1.8,0
                     set xlab 'Distance parcouru' font 'Arial, 15' offset 2,0
-
-                    set xrange [100000:160000]
-                    set yrange [-0.5:8.5]
-                    set ytics rotate by 60 offset 1,.2 right
+                    set yrange [-0.75:9.75]
 
 
-                    plot 'temp/distance2.txt' u (\$2/2):0:(\$2/2):(0.35/2.):ytic(1) w boxxy ti 'Distance en KM'" > temp/distance.gp
+                    plot 'temp/distance2.txt' u (\$2/2):0:(\$2/2):(0.75/2.):ytic(1) w boxxy ti 'Distance en KM' lc 'blue'" > temp/distance.gp
         
         gnuplot temp/distance.gp
         ;;
@@ -113,22 +112,18 @@ case "$2" in
         echo   "set terminal pngcairo enhanced font 'arial,10' size 800,600
                 set output 'images/trajet_distance.png'
                 set datafile separator ';'
+                unset key
 
                 set style data histogram
-                set style fill solid border -1
+                set style fill solid 1
+                set boxwidth 1.9
+                set margin 10,2,4,4
 
                 set title 'Histogramme Vertical des Distances de Trajets' font 'Arial, 15'
                 set xlabel 'Identifiant du Trajet' font 'Arial, 12' offset 0,-0.3
                 set ylabel 'Distance (km)' font 'Arial, 12' offset 0.3,0
-                set style fill solid 1
-                set boxwidth 0.8
 
-                set lmargin 10
-                set rmargin 2
-                set tmargin 4
-                set bmargin 4
-
-                plot 'temp/trajet1.txt' u 2:xtic(1) title 'Nbr Km' lc 'red'" > temp/trajet2.gp
+                plot 'temp/trajet1.txt' u 2:xtic(1) title 'Nbr Km' lc 'blue'" > temp/trajet2.gp
 
         gnuplot temp/trajet2.gp
         ;;
